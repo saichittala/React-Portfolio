b8b6e6d3b4c4d26fc2ba5958e33d590e4dab446a
saichittala <sai.chittala@gmail.com>
2024-11-22 03:12:02 +0530

    final changes of react

--- src/mydeziner.js
+++ src/mydeziner.js
@@ -1,8 +1,8 @@
 import React, { useEffect} from 'react';
-import Header from '../components/header';
-import Footer from '../components/footer';
-import useFadeIn from '../components/useFadeIn';
-import useScrollEffect from '../components/useScrollEffect';
+import Header from './components/header';
+import Footer from './components/footer';
+import useFadeIn from './components/useFadeIn';
+import useScrollEffect from './components/useScrollEffect';
 
 
 
