1419ebf19c49d6b913373edde3082464166389f2
saichittala <sai.chittala@gmail.com>
2024-11-22 18:54:46 +0530

    Fixed Routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Home from './home.js';
 import MyDeziner from './pages/mydeziner.js';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {HashRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter >
+    <HashRouter >
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mydeziner" element={<MyDeziner />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 }
