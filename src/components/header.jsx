f94be252ccb10a642e290764f63d61f14dff3e5f
saichittala <sai.chittala@gmail.com>
2025-01-12 11:40:09 +0530

    header changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -27,14 +27,16 @@ function Header() {
           {/* Mobile Menu Container */}
           <div className={`menu-container ${menuActive ? 'active' : ''}`} id="menu-container">
             <div className="mob-nav-btns">
-              <a href="/#about" className="about-button btn-1 mob-btn-1">About</a>
+              <a href="." className="about-button btn-1 mob-btn-1">Work</a>
+              <a href="/#about" className="about-button btn-1 mob-btn-1">Info</a>
               <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer" className="btn-1 mob-btn-1">Resume</a>
             </div>
           </div>
 
           {/* Desktop Navigation */}
           <div className="nav-btns">
-            <a href="/#about" className="about-button header-text">About</a>
+            <a href="." className="about-button header-text">Work</a>
+            <a href="/#about" className="about-button header-text">Info</a>
             <a href="Resume.pdf" target="_blank" rel="noopener noreferrer" className="header-text">Resume</a>
           </div>
         </div>
