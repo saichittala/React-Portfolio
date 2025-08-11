0c8117070ccd72bbfab3f17f3eb47282992d3c06
saichittala <sai.chittala@gmail.com>
2025-08-11 20:29:03 +0530

    new changes

--- src/pages/customfurnish.js
+++ src/pages/customfurnish.js
@@ -111,7 +111,7 @@ const CustomFurnish = () => {
               hideDelay={500}
               size={{
                 small: 12,
-                large: { width: 221, height: 52 }
+                large: { width: 220.65, height: 52 }
               }}
               activateAt={isMobile ? -3.8 : -0.5}
               className="custom-bubble-class" >
