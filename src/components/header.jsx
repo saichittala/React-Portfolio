43ca2211348ab33c3056f85b20a5e87ab25b301f
saichittala <110885282+saichittala@users.noreply.github.com>
2026-01-25 23:22:25 +0530

    new chanegs

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -28,81 +28,71 @@ function Header({ toggleTheme, theme }) {
 
   return (
     <div>
-      <nav className="nav-bar">
-        <div className="nav-main">
-          {/* Menu Icon */}
-          {/* <img
+      <nav className="nav-bar width-nav">
+
+        <div className='df-g8 jc-sb'>
+          <div className='df-g8 alc logo'>
+            <img src="img/logo-main.svg" alt="" className='logo' />
+          </div>
+          <div className="nav-main">
+            {/* Menu Icon */}
+            <img
             className={`menu-icon ${menuActive ? 'clicked' : ''}`}
             src={menuActive ? 'img/close.svg' : 'img/menu.svg'}
             alt="menu-icon"
             onClick={toggleMenu}
-          /> */}
+          />
 
-          {/* Mobile Menu Container - Updated to use Link */}
-          {/* <div className={`menu-container ${menuActive ? 'active' : ''}`} id="menu-container">
+            {/* Mobile Menu Container - Updated to use Link */}
+            <div className={`menu-container ${menuActive ? 'active' : ''}`} id="menu-container">
             <div className="mob-nav-btns">
               <Link to="/works" className="about-button btn-1 mob-btn-1">Work</Link>
               <Link to="/about" className="about-button btn-1 mob-btn-1 ">Info</Link>
             </div>
-          </div> */}
+          </div>
 
-          {/* Desktop Navigation - Updated to use Link and dynamic classes */}
-          <div className="nav-btns">
-            {/* The 'Home' link is active if the path is exactly "/" */}
-            <Link to="/" className={`about-button header-text ${location.pathname === '/' ? 'header-text-active' : ''}`}>
-              {/* <lord-icon
-                ref={iconRef}
-                src="https://cdn.lordicon.com/szzviyeh.json"
-                trigger="hover"
-                style={{ width: "28px", height: "28px" }}
-                className="home-icon"
-              ></lord-icon> */}
-              <img src="img/home-nav.svg" alt="logo" />
+            {/* Desktop Navigation - Updated to use Link and dynamic classes */}
+            <div className="nav-btns">
+              {/* The 'Home' link is active if the path is exactly "/" */}
+              <Link to="/" className={`about-button menu-nav header-text-1 ${location.pathname === '/' ? 'header-text-active' : ''}`}>
+                <img src="img/home-nav.svg" alt="logo" />
 
-              <span>Home</span>
-            </Link>
+                <span>Home</span>
+              </Link>
 
-            {/* The 'Works' link is active if the path is "/works" */}
-            <Link to="/works" className={`about-button header-text ${location.pathname === '/works' ? 'header-text-active' : ''}`}>
-              {/* <lord-icon
-                ref={iconRef}
-                src="https://cdn.lordicon.com/rrfthkgx.json"
-                trigger="hover"
-                style={{ width: "28px", height: "28px" }}
-                className="home-icon"
-                colors="primary:#737373,secondary:#08a88a"
+              {/* The 'Works' link is active if the path is "/works" */}
+              <Link to="/works" className={`about-button menu-nav header-text-1 ${location.pathname === '/works' ? 'header-text-active' : ''}`}>
+                <img src="img/works-nav.svg" alt="logo" />
+                <span>Works</span>
+              </Link>
 
-              ></lord-icon> */}
-              <img src="img/works-nav.svg" alt="logo" />
-              <span>Works</span>
-            </Link>
+              {/* The 'About' link is active if the path is "/about" */}
+              <Link to="/about" className={`about-button menu-nav header-text-1 ${location.pathname === '/about' ? 'header-text-active' : ''}`}>
+                <img src="img/profile-nav.svg" alt="logo" />
+                <span>About</span>
+              </Link>
 
-            {/* The 'About' link is active if the path is "/about" */}
-            <Link to="/about" className={`about-button header-text ${location.pathname === '/about' ? 'header-text-active' : ''}`}>
-              <img src="img/profile-nav.svg" alt="logo" />
-              <span>About</span>
-            </Link>
-
-            {/* The 'Contact' link is active if the path is "/contact" */}
-            <Link to="/contact" className={`about-button header-text ${location.pathname === '/contact' ? 'header-text-active' : ''}`}>
-              <img src="img/contact-nav.svg" alt="logo" />
-              <span>Contact</span>
-            </Link>
-            <div className='df-g8 aic'>
-              <div className="divider"></div>
-            </div>
-            <button
-              className="theme-toggle-btn"
-              onClick={toggleTheme}
-            >
-              {theme === "light" ? 
-              <div className='header-text padding-4'>
-                <img  src="img/dark-nav.svg" alt="Light Mode" />  
+              {/* The 'Contact' link is active if the path is "/contact" */}
+              <Link to="/contact" className={`about-button menu-nav header-text-1 ${location.pathname === '/contact' ? 'header-text-active' : ''}`}>
+                <img src="img/contact-nav.svg" alt="logo" />
+                <span>Contact</span>
+              </Link>
+              <div className='df-g8 aic'>
+                <div className="divider"></div>
               </div>
-               : <div className='header-text padding-4'>
-                   <img src="img/light-nav.svg" alt="Dark Mode" />
-                 </div>}
-            </button>
+              <button
+                className="theme-toggle-btn"
+                onClick={toggleTheme}
+              >
+                {theme === "light" ?
+                  <div className='header-text padding-4'>
+                    <img src="img/dark-nav.svg" alt="Light Mode" />
+                  </div>
+                  : <div className='header-text padding-4'>
+                    <img src="img/light-nav.svg" alt="Dark Mode" />
+                  </div>}
+              </button>
+            </div>
 
           </div>
         </div>
